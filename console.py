691f62bf064c0878789391e0027b342d26b5cc63
Santos Edward <santosedward1998@gmail.com>
2024-02-11 08:07:30 -0800

    update

--- console.py
+++ console.py
@@ -201,14 +201,14 @@ class HBNBCommand(cmd.Cmd):
                     obj.__dict__[argl[2]] = argl[3]
             elif type(eval(argl[2])) == dict:
                 obj = objdict["{}.{}".format(argl[0], argl[1])]
-                for i, j in eval(argl[2]).items():
-                    if (i in obj.__class__.__dict__.keys() and
-                            type(obj.__class__.__dict__[i]) in
+                for k, v in eval(argl[2]).items():
+                    if (k in obj.__class__.__dict__.keys() and
+                            type(obj.__class__.__dict__[k]) in
                             {str, int, float}):
-                        valtype = type(obj.__class__.__dict__[i])
-                        obj.__dict__[i] = valtype(j)
+                        valtype = type(obj.__class__.__dict__[k])
+                        obj.__dict__[k] = valtype(v)
                     else:
-                        obj.__dict__[i] = j
+                        obj.__dict__[k] = v
             storage.save()
 
 
